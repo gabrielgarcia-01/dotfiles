1dcc4f0bdcd6b72aecf06da6b57b8fc93d2161dd
gabrielgarcia-01 <122548028+gabrielgarcia-01@users.noreply.github.com>
2023-05-26 18:43:03 -0400

    init repo, several themes for my configs

---
+++ bash/.bashrc.d/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# ----------------------------------------------------------------------------------
+# system info
+nitch
+
+# starship prompt
+eval "$(starship init bash)"
+
+# zoxide
+eval "$(zoxide init bash)"
---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+local opt = vim.opt
+
+vim.g.gruvbox_material_transparent_background = 1
+vim.g.gruvbox_material_enable_italic = 1
+vim.g.gruvbox_material_enable_bold = 1
+vim.g.gruvbox_material_background = "soft"
